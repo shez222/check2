be167b3d73badfb2b1a10dbaea78e19d5886229f
shez222 <bilal.shehroz420@gmail.com>
2024-08-18 08:30:05 +0500

    changevv

--- ghhgjh/app/firebase.js
+++ ghhgjh/app/firebase.js
@@ -6,13 +6,13 @@ import { getAppCheck, initializeAppCheck, ReCaptchaV3Provider } from "firebase/a
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAr-yQEsSPLMdinQ8GHITRYFUbiTwd8KAc",
+  authDomain: "school-web-c3ed5.firebaseapp.com",
+  projectId: "school-web-c3ed5",
+  storageBucket: "school-web-c3ed5.appspot.com",
+  messagingSenderId: "1066637057144",
+  appId: "1:1066637057144:web:504912fab65a5f2333e18d",
+  measurementId: "G-H26P0TE4N0"
 };
 
 // Initialize Firebase
@@ -23,7 +23,7 @@ const auth = getAuth(app);
 var appCheck;
 if (typeof window !== "undefined") {
   appCheck = initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY),
+    provider: new ReCaptchaV3Provider("6LeBjBgqAAAAAFNi6Qk-ByG99-VQY0Qdcrl7J-ST"),
     isTokenAutoRefreshEnabled: true
   });
 }
